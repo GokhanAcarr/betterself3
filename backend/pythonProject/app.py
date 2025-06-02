827b3e1f1ef42747d7f83619265d0d890bf09744
Gökhan Acar <gokhannacarr@hotmail.com>
2025-06-02 16:23:19 +0300

    tryy

--- backend/pythonProject/app.py
+++ backend/pythonProject/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from extensions import db, jwt, cors
 from datetime import timedelta
@@ -9,25 +11,26 @@ from routes.food import food_bp
 from routes.post import post_bp
 from routes.sleep import sleep_bp
 from routes.water import water_bp
-import os
 
 def create_app():
     app = Flask(__name__)
 
-    # Konfigürasyonlar
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
-
-
+    # DB URI doğrudan buraya yazıldı (env kullanmıyoruz)
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"mysql://{os.environ.get('MYSQLUSER')}:{os.environ.get('MYSQLPASSWORD')}"
+    f"@{os.environ.get('MYSQLHOST')}:{os.environ.get('MYSQLPORT')}/{os.environ.get('MYSQLDATABASE')}"
+)
+    
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'secret-key'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
 
-    # Extensions
+    # Extensions başlat
     db.init_app(app)
     jwt.init_app(app)
     cors.init_app(app, resources={r"/*": {"origins": "*"}})
 
-    # Blueprint kayıtları
+    # Blueprintleri kaydet
     app.register_blueprint(auth_bp)
     app.register_blueprint(exercise_bp)
     app.register_blueprint(food_bp)
@@ -35,7 +38,6 @@ def create_app():
     app.register_blueprint(sleep_bp)
     app.register_blueprint(water_bp)
 
-    # Veritabanını oluştur
     with app.app_context():
         db.create_all()
 
@@ -43,4 +45,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
